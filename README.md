81738bafd9f8052b1188f0835ff8a93ec8824e26
allisonbusse <allisonbusse@gmail.com>
2019-10-31 16:28:00 -0700

    cleanup and readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-Decks and Cards
+# Decks and Cards
+This lab goes through several iterations of creating Deck and Card components to render in App. We moved the rendering out of App and into separate components, then later created Character and Gif components that fetched the data from the Hey Arnold API. 
--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,12 @@
 import React from 'react';
-// import { characters, gifs } from '../content.json';
-// import DeckOfComps from '../containers/DeckOfComps';
-// import Card from './Card';
-// import Deck from '../containers/Deck';
 import Characters from '../containers/Characters';
 import Gifs from '../containers/Gifs';
 
 export default function App() {
   return (
     <>
-      <Characters
-        count={5}
-      />
-      <Gifs
-        count={2}
-      />
-
-      {/* <DeckOfComps
-        title="Characters"
-        items={characters.map(character => ({ 
-          ...character, 
-          title: character.name, 
-          content: `${character.name} has ${character._id}`
-        }))}
-        Component={Card}
-      />
-      <DeckOfComps
-        title="GIFs"
-        items={gifs.map(gif => ({ 
-          ...gif, 
-          title: gif._id
-        }))}
-        Component={Card}
-      />
-
-
-      <Deck title="Characters">
-        {characters.map(({ _id, name, image }) => (
-          <Card 
-            key={_id} 
-            title={name}
-            image={image}
-            content={`${name} has ${_id}`}
-          />
-        ))}
-      </Deck>
-      <Deck title="GIFs">
-        {gifs.map(({ _id, image }) => (
-          <Card 
-            key={_id} 
-            title={_id}
-            image={image}
-          />
-        ))}
-      </Deck> */}
+      <Characters count={5}/>
+      <Gifs count={2}/>
     </>
   );
 }
