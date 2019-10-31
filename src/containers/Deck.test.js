fb3732451d29df411d91a2862ba79a831d255370
Dave Trost <davetrost@gmail.com>
2019-10-31 15:50:48 -0700

    tested Deck component

---
+++ src/containers/Deck.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Deck from './Deck';
+
+describe('Deck component', () => {
+  it('renders', () => {
+    const wrapper = shallow(
+      <Deck title="Test Title">
+        <p>card 1</p>
+        <p>card 2</p>
+      </Deck>
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
